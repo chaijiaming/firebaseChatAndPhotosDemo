59ef674dbf5c838460c784ff77bdeddeaf5b6302
chaijiaming <chaijiaming@vip.qq.com>
2017-05-15 16:53:13 +0800

    Initial Commit

---
+++ firesbaseDemoChats/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firesbaseDemoChats
+//
+//  Created by Jeremy Chai on 5/15/17.
+//  Copyright © 2017 JiamingChai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
